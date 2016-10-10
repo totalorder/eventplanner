5357abc8e4727fe8a84b4cfbf83601ff778896e1
Anton Blomberg <anton@deadlock.se>
2016-10-10 21:26:27 +0200

    Add support for CSOs to create and approve events

--- proj/app/models.py
+++ proj/app/models.py
@@ -1,5 +1,33 @@
 from __future__ import unicode_literals
 
+from django.forms.extras import SelectDateWidget
 from django.db import models
+from django.forms import ModelForm, fields
 
-# Create your models here.
+
+class PlanningRequest(models.Model):
+    client_name = models.CharField(max_length=128)
+    event_type = models.CharField(max_length=128)
+    from_date = models.DateField()
+    to_date = models.DateField()
+    expected_no_attending = models.IntegerField()
+    decoration = models.BooleanField()
+    parties = models.BooleanField()
+    drinks = models.BooleanField()
+    food = models.BooleanField()
+    media = models.BooleanField()
+    expected_budget = models.IntegerField()
+    state = models.CharField(max_length=15, choices=(
+        ("new", "New"),
+        ("cso_approved", "CSO Approved"),
+        ("fm_commented", "FM Commented"),
+    ), default="new")
+
+
+class PlanningRequestForm(ModelForm):
+    class Meta:
+        model = PlanningRequest
+        exclude = ["state"]
+
+    from_date = fields.DateField(widget=SelectDateWidget())
+    to_date = fields.DateField(widget=SelectDateWidget())
--- proj/app/views.py
+++ proj/app/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse, reverse_lazy
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login as _login, logout as _logout
+from app import models
 
 
 def login(request):
@@ -21,6 +23,42 @@ def logout(request):
     return redirect(reverse("login"))
 
 
+def planning_request(request):
+    if not in_group(request.user, "cso"):
+        return HttpResponse("You are not authorized to access this page", status=401)
+    planning_request_form = models.PlanningRequestForm(request.POST)
+
+    if request.method == "POST":
+        if planning_request_form.is_valid():
+            planning_request_form.save()
+
+    return render(request, "planning_request.html", {
+        "form": planning_request_form,
+        "planning_requests": models_to_dicts(models.PlanningRequest.objects.all())
+    })
+
+
+def model_to_dict(obj):
+    obj.dict = {(field.attname, getattr(obj, field.attname)) for field in obj._meta.fields}
+    return obj
+
+
+def models_to_dicts(objs):
+    return [model_to_dict(obj) for obj in objs]
+
+
+def in_group(user, group_name):
+    return user.groups.filter(name=group_name).count() > 0
+
+
 @login_required(login_url=reverse_lazy("login"))
 def index(request):
-    return render(request, "index.html")
+    return render(request, "index.html", {"groups": request.user })
+
+
+def cso_approve(request):
+    planning_request = models.PlanningRequest.objects.get(
+        pk=request.POST.get("planning_request_id"))
+    planning_request.state = "cso_approved"
+    planning_request.save()
+    return redirect("planning_request")
